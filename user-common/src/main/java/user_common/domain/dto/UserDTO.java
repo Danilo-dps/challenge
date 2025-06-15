29d4785c3e029bbe90b7d76582a30be37500654d
Danilo-dps <danilodpsnilo@outlook.com>
2025-06-15 14:20:49 -0300

    feat: garantindo que os campos email e cpf sejam válidos

--- user-common/src/main/java/user_common/domain/dto/UserDTO.java
+++ user-common/src/main/java/user_common/domain/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package user_common.domain.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.UUID;
 
@@ -13,7 +16,15 @@ import java.util.UUID;
 public class UserDTO {
 
     private UUID idUser;
+
+    @NotBlank(message = "O nome completo é obrigatório")
     private String fullName;
+
+    @CPF(message = "CPF inválido")
+    @NotBlank(message = "O CPF é obrigatório")
     private String cpf;
+
+    @Email(message = "Formato de e-mail inválido")
+    @NotBlank(message = "O e-mail é obrigatório")
     private String email;
 }
