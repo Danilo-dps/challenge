0b58c39ae44ffd7261f87e6861be88bcfb37a4d2
danilo <danilodpsnilo@outlook.com>
2025-06-16 21:14:28 -0300

    feat: logger adicionado para melhorar o debug

--- user-common/src/main/java/user_common/domain/service/impl/UserServiceImpl.java
+++ user-common/src/main/java/user_common/domain/service/impl/UserServiceImpl.java
@@ -27,29 +27,34 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserDTO create(UserDTO userDTO) {
-
+        logger.info("Criando login...");
         if (userDTO.getFullName() == null || userDTO.getFullName().trim().isEmpty()) {
+            logger.warning("Erro. Nome está vazio");
             throw new UserNameEmptyException();
         }
 
         if (userDTO.getCpf() == null || userDTO.getCpf().trim().isEmpty()) {
+            logger.warning("Erro. CPF está vazio");
             throw new UserCPFEmptyException();
         }
 
         if (userDTO.getEmail() == null || userDTO.getEmail().trim().isEmpty()) {
+            logger.warning("Erro. Email está vazio");
             throw new UserEmailEmptyException();
         }
 
         if (userRepository.findByCpf(userDTO.getCpf()).isPresent()) {
+            logger.warning("Erro. CPF já cadastrado");
             throw new DuplicateCPFException(userDTO.getCpf());
         }
 
         if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
+            logger.warning("Erro. Email já cadastrado");
             throw new DuplicateEmailException(userDTO.getEmail());
         }
 
         User user = User.builder().fullName(userDTO.getFullName()).email(userDTO.getEmail()).cpf(userDTO.getCpf()).build();
-        logger.info("Usuário criado");
+        logger.info("Usuário criado!");
         User savedUser = userRepository.save(user);
         return User2UserDTO.convert(savedUser);
     }
@@ -58,20 +63,24 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public UserDTO getById(UUID userId) {
         Objects.requireNonNull(userId, "User ID não pode ser null");
-
+        logger.info("Procurando usuário...");
         return userRepository.findById(userId)
                 .map(User2UserDTO::convert)
-                .orElseThrow(() -> new UserNotFoundException(userId));
+                .orElseThrow(() -> {
+                    logger.warning("Usuário não encontrado com ID: " + userId);
+                    return new UserNotFoundException(userId);
+                });
     }
 
     @Override
     public UserDTO update(UUID userId, UserResponse userResponse) {
-
-        User existingUser = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+        logger.info("Atualizando dados...");
+        User existingUser = userRepository.findById(userId).orElseThrow(() -> {logger.warning("Usuário não encontrado com ID: " + userId); return new UserNotFoundException(userId);});
 
         if (userResponse.email() != null
                 && !userResponse.email().equals(existingUser.getEmail())
                 && userRepository.findByEmail(userResponse.email()).isPresent()) {
+                logger.warning("Erro. email já cadastrado");
                 throw new DuplicateEmailException(userResponse.email());
         }
 
@@ -90,7 +99,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(UUID userId) {
-        logger.info("Verificando a existência do usuário para excluir");
+        logger.info("Verificando a existência do usuário para excluir...");
         if (!userRepository.existsById(userId)) {
             logger.warning("Erro. Usuário não encontrado");
             throw new UserNotFoundException(userId);
