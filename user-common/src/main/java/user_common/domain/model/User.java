e771d604032566fe4cee98cfe2da5e7be02311d1
danilo <danilodpsnilo@outlook.com>
2025-06-16 23:51:39 -0300

    feat: atributo balance e método para depósito adicionado

--- user-common/src/main/java/user_common/domain/model/User.java
+++ user-common/src/main/java/user_common/domain/model/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
@@ -39,4 +40,13 @@ public class User implements Serializable {
 
     @Column(nullable = false, unique = true, length = 50)
     private String email;
+
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    public void deposit(BigDecimal value) {
+        if (value == null || value.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Valor do depósito deve ser positivo");
+        }
+        this.balance = this.balance.add(value);
+    }
 }
