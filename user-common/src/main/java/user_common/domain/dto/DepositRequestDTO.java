ad6f6afe22e27817f380488897add3bc993c0767
danilo <danilodpsnilo@outlook.com>
2025-06-20 22:29:39 -0300

    feat: criando entidades e DTOs para separação de registros

---
+++ user-common/src/main/java/user_common/domain/dto/DepositRequestDTO.java
@@ -0,0 +1,16 @@
+package user_common.domain.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositRequestDTO {
+    private BigDecimal amount;
+    private String userEmail;
+}
